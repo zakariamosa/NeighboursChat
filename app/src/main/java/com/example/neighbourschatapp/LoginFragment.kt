a18aa18d90c4f9124c83450b0988beee29bba800
Olof Hammar <olle_hammat@hotmail.com>
2020-11-02 11:45:03 +0100

    Added transaction between login and register fragments.

--- app/src/main/java/com/example/neighbourschatapp/LoginFragment.kt
+++ app/src/main/java/com/example/neighbourschatapp/LoginFragment.kt
@@ -5,11 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentTransaction
 
 class LoginFragment: Fragment() {
     @ExperimentalStdlibApi
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val tvAlreadyHaveAccount: TextView = view.findViewById(R.id.go_to_register_textview)
+
+        tvAlreadyHaveAccount.setOnClickListener {
+
+            val transaction: FragmentTransaction = this.fragmentManager!!.beginTransaction()
+            val frag: Fragment = RegisterFragment()
+            transaction.replace(R.id.main_fragment_layout, frag)
+            transaction.commit()
+        }
 
         return view
     }
--- app/src/main/java/com/example/neighbourschatapp/RegisterFragment.kt
+++ app/src/main/java/com/example/neighbourschatapp/RegisterFragment.kt
@@ -5,11 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.FragmentTransaction
 
 class RegisterFragment: Fragment() {
     @ExperimentalStdlibApi
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_register, container, false)
+
+        val tvBackToLogin: TextView = view.findViewById(R.id.back_to_login)
+
+        tvBackToLogin.setOnClickListener {
+
+            val transaction: FragmentTransaction = this.fragmentManager!!.beginTransaction()
+            val frag: Fragment = LoginFragment()
+            transaction.replace(R.id.main_fragment_layout, frag)
+            transaction.commit()
+        }
 
         return view
     }
