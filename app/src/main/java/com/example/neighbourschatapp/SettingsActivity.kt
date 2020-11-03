a5e45879489c707a68070e5b59e4403e12e9202c
Olof Hammar <olle_hammat@hotmail.com>
2020-11-03 13:06:23 +0100

    Added function to verify login when app starts. Added textviews to show what is coming in each activity.

--- app/src/main/java/com/example/neighbourschatapp/SettingsActivity.kt
+++ app/src/main/java/com/example/neighbourschatapp/SettingsActivity.kt
@@ -1,14 +1,28 @@
 package com.example.neighbourschatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        supportActionBar?.title = "Settings"
+        val btnSignOut = findViewById<TextView>(R.id.btn_sign_out)
 
+        btnSignOut.setOnClickListener {
+            signOut()
+        }
+
+        supportActionBar?.title = "Settings"
+    }
+    private fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 }
