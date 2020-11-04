16b807bc2fe4a6c94218e1af698b9c857311452c
Olof Hammar <olle_hammat@hotmail.com>
2020-11-04 13:18:52 +0100

    Moved fetchUser(). Now it imports user-list every time.

--- app/src/main/java/com/example/neighbourschatapp/NewMessageActivity.kt
+++ app/src/main/java/com/example/neighbourschatapp/NewMessageActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
+import android.os.PersistableBundle
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -58,22 +59,20 @@ class NewMessageActivity : AppCompatActivity() {
                     currentuserlat = location.latitude
                     currentuserlong = location.longitude
                     Log.d("!!!", "last location lat: $currentuserlat, lng: $currentuserlong")
+                    fetchUsers()
                     //will save this in user table
                 }
             }
-
         }
 
         locationRequest = creatLocationRequest()
-
-        fetchUsers()
     }
     //Denna funktion laddar alla registrerade användare i en recyclerview i realtid, men laddar listan två gånger
     private fun fetchUsers() {
 
         val db = FirebaseFirestore.getInstance()
         val adapter = GroupAdapter <ViewHolder>()
-        var currentUser = FirebaseAuth.getInstance().currentUser
+        val currentUser = FirebaseAuth.getInstance().currentUser
         val itemRef = db.collection("users")
 
         itemRef.addSnapshotListener {snapshot, e ->
@@ -83,6 +82,7 @@ class NewMessageActivity : AppCompatActivity() {
 
                     val user = document.toObject(User::class.java)
                     if (user != null && currentUser?.uid != user.userId) {
+                        Log.d("!!!!", "$currentuserlat and $currentuserlong")
                         if (isneighbours(user)){
                             adapter.add(UserItem(user))
                         }
@@ -102,7 +102,6 @@ class NewMessageActivity : AppCompatActivity() {
         }
     }
 
-    //Denna delen hanterar location
     override fun onResume() {
         super.onResume()
 
@@ -115,13 +114,11 @@ class NewMessageActivity : AppCompatActivity() {
         stopLocationUpdates()
     }
 
-
     fun startLocationUpdates() {
         if( ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED) {
             locationProvider.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
         }
-
     }
 
     fun stopLocationUpdates() {
@@ -135,8 +132,6 @@ class NewMessageActivity : AppCompatActivity() {
                 priority = LocationRequest.PRIORITY_HIGH_ACCURACY
             }
 
-
-
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if(requestCode == REQUEST_LOCATION ) {
             if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
@@ -146,7 +141,6 @@ class NewMessageActivity : AppCompatActivity() {
                 Log.d("!!!", "Permission denied")
             }
         }
-
     }
 
     //Here getting distance in kilometers (km)
@@ -173,12 +167,10 @@ class NewMessageActivity : AppCompatActivity() {
 
     private fun isneighbours(usr:User):Boolean{
 
-
         val distancefrommeinkm=distance(currentuserlat,currentuserlong,usr.lastLocationLat,usr.lastLocationLong)
         if (distancefrommeinkm<5){
             return true
         }
         return false
-
     }
 }
