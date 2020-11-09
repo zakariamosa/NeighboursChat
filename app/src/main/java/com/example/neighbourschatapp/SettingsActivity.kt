8b54ff12ab322ae817a95893ff0ee931fe6d331a
zakariamosa <zakaria.mosa@iths.se>
2020-11-09 23:31:56 +0100

    added Location to settings in fragment

--- app/src/main/java/com/example/neighbourschatapp/SettingsActivity.kt
+++ app/src/main/java/com/example/neighbourschatapp/SettingsActivity.kt
@@ -20,47 +20,38 @@ class SettingsActivity : AppCompatActivity() {
 
 
 
-        locationDistance=findViewById(R.id.editTextNumberLocationDistance)
+
         val btnSignOut = findViewById<TextView>(R.id.btn_sign_out)
         btnSignOut.setOnClickListener {
             signOut()
         }
 
-        val btnSaveSettings=findViewById<Button>(R.id.buttonSaveSettings)
-        btnSaveSettings.setOnClickListener(){
-            saveSettings(locationDistance.text.toString().toInt())
+
+
+
+
+        val buttonNeighbourDistanceSetting=findViewById<Button>(R.id.buttonNeighbourDistanceSetting)
+        buttonNeighbourDistanceSetting.setOnClickListener(){
+            callNeighbourDistanceSetting()
         }
 
-        loadUserSettings()
 
         supportActionBar?.title = "Settings"
     }
 
-    private fun loadUserSettings() {
-        val db = FirebaseFirestore.getInstance()
-        val currentUser = FirebaseAuth.getInstance().currentUser
-        val itemRef = db.collection("Settings").document(currentUser!!.uid).get()
-        itemRef.addOnCompleteListener(){
-            if (it.isSuccessful){
-                locationDistance.setText(it.result.data?.get("locationDistance").toString())
+    private fun callNeighbourDistanceSetting() {
 
-            }
-        }
+        val neighbourDistanceSettingFragment =  fragment_neighbourdistance.newInstance("","")
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.container, neighbourDistanceSettingFragment, "neighbourdistance" )
+
+        transaction.commit()
     }
 
-    private fun saveSettings(locationDistance:Int) {
-        val userId = FirebaseAuth.getInstance().uid ?: ""
-        val db = FirebaseFirestore.getInstance()
-        val userSettings = UserSettings(locationDistance)
 
-        db.collection("Settings").document(userId).set(userSettings)
-                .addOnSuccessListener {
 
-                    val intent = Intent(this, NewMessageActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    startActivity(intent)
-                }
-    }
+
 
     private fun signOut() {
         FirebaseAuth.getInstance().signOut()
---
+++ app/src/main/java/com/example/neighbourschatapp/fragment_neighbourdistance.kt
@@ -0,0 +1,100 @@
+package com.example.neighbourschatapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [fragment_neighbourdistance.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class fragment_neighbourdistance : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    lateinit var locationDistance:EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment
+        val myview= inflater.inflate(R.layout.fragment_neighbourdistance, container, false)
+
+        locationDistance=myview.findViewById(R.id.editTextNumberNeighbourLocationDistance)
+        loadUserSettings()
+        val btnSaveSettings=myview.findViewById<Button>(R.id.buttonSaveNeighbourLocationDistance)
+        btnSaveSettings.setOnClickListener(){
+            saveSettings(locationDistance.text.toString().toInt())
+        }
+
+
+        return myview
+    }
+
+    private fun loadUserSettings() {
+
+        val db = FirebaseFirestore.getInstance()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val itemRef = db.collection("Settings").document(currentUser!!.uid).get()
+        itemRef.addOnCompleteListener(){
+            if (it.isSuccessful){
+                locationDistance.setText(it.result.data?.get("locationDistance").toString())
+
+            }
+        }
+    }
+
+    private fun saveSettings(locationDistance:Int) {
+        val userId = FirebaseAuth.getInstance().uid ?: ""
+        val db = FirebaseFirestore.getInstance()
+        val userSettings = UserSettings(locationDistance)
+
+        db.collection("Settings").document(userId).set(userSettings)
+            .addOnSuccessListener {
+
+                Toast.makeText(this@fragment_neighbourdistance.context, "Saved Successfully", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment fragment_neighbourdistance.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+                fragment_neighbourdistance().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
+}
