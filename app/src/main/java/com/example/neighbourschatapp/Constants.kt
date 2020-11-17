ddb5aea2723099d3874852735fd2ba334cdb11d3
zakariamosa <zakaria.mosa@iths.se>
2020-11-17 17:45:57 +0100

    returned back sad

--- app/src/main/java/com/example/neighbourschatapp/Constants.kt
+++ app/src/main/java/com/example/neighbourschatapp/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAVp_8jFk:APA91bHpck_v08kN0WiSitQv0SEgsF3hL2air59qgxBl1JP-ukh2mnFFZh_3BJmf57wS_3Rk6ck_xtRpgQAgVZKfBbETvQs8b4uV1ZY3efazh0lg-k6EOLaQwDgMIR310rV2GR9PvOuB"
+        const val SERVER_KEY = "AAAAHiWNYEQ:APA91bH5pfnM5hstpsIZ-pURaIwI8FFSHBItbSEXsAyau1VqIIlTuvWjWkLEdeqbo31eQ814tNDnY08msQ8i8a9eOUnzJ-GbW_xzrVgFm3naXJD3n9sLP6peMhVG9j5qPjOwbUJskrFW"
         const val CONTENT_TYPE = "application/json"
     }
 }
