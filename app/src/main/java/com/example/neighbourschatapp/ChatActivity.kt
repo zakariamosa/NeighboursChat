dda1a58873d3741176d198af52757e44c3ab0fe9
Olof Hammar <olle_hammat@hotmail.com>
2020-11-30 12:56:04 +0100

    Optimized imports.

--- app/src/main/java/com/example/neighbourschatapp/ChatActivity.kt
+++ app/src/main/java/com/example/neighbourschatapp/ChatActivity.kt
@@ -3,31 +3,19 @@ package com.example.neighbourschatapp
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.provider.ContactsContract
-import android.provider.DocumentsContract
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.SortedList
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
-import com.google.firebase.database.Query
-import com.google.firebase.firestore.*
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
-import kotlinx.android.synthetic.main.activity_chat.*
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 
 class ChatActivity : AppCompatActivity() {
